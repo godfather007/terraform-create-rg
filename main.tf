062095a673ab8e3c319e7e4a810f85bee99e59fd
abhay <abhay.ciem@gmail.com>
2020-06-07 14:52:22 +0530

    commit and terraform files

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "azurerm_resource_group" "resource_gp" {
+  name = "skylines_Demo_5"
+  location = "eastus"
+  tags = {
+      Owner = "Abhay Kumar"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+subscription_id ="${var.subscription_id}"
+client_id = "${var.client_id}"
+client_secret = "${var.client_secret}"
+tenant_id = "${var.tenant_id}"
+  features {}
+}
+
+variable "subscription_id" {
+  description = "Enter subscription_id"
+}
+
+variable "client_id" {
+  description = "Enter client_id"
+}
+variable "client_secret" {
+  description = "Enter client_secret"
+}
+variable "tenant_id" {
+  description = "Enter tenant_id"
+}
+
